9f55311b60810bee6ba83a268167ea0c0f5c3ebe
arojitmondal996e <arojitmondal996@gmail.com>
2024-11-28 13:52:29 +0600

    Add toast icon

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 const AddCoffee = () => {
 
     const handleAddCoffee = event => {
@@ -14,7 +16,29 @@ const AddCoffee = () => {
         const photo = form.photo.value;
 
         const newCoffee = {name, quantity, supplier, taste, category, details, photo};
-        console.log(newCoffee)
+        
+        console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Coffee added successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
+        })
     }
     return (
         <div className='bg-[#f4f3f0] p-24'>
